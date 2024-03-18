28a1ac7594848a7dc6099799e35a802503e5fe10
Edward <edwardthupane80@gmail.com>
2024-03-18 15:37:58 +0200

    borrower

--- src/app/tables/borrower-table/borrower-table.component.ts
+++ src/app/tables/borrower-table/borrower-table.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-borrower-table',
@@ -10,6 +11,17 @@ export class BorrowerTableComponent {
   displayedColumns: string[] = ['loanType', 'amount', 'status'];
   status: string[] = ['in-progress', 'approved', 'declined']
   dataSource = new MatTableDataSource<any>();
+
+  constructor(private api:ApiService){
+    this.api.genericGet('/get-loans')
+      .subscribe({
+        next: (res: any) => {
+          this.dataSource = res
+        },
+        error: (err: any) => console.log('Error', err),
+        complete: () => { }
+      });
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
